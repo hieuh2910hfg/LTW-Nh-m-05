a7ed766436f392c4ec841099433725f8f9a9632d
Tran Duy Hieu <h2910hfg123789@gmail.com>
2024-11-28 02:54:37 +0700

    Update DBConnection.java

--- BTL/src/main/java/Admin/DBConnection.java
+++ BTL/src/main/java/Admin/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ql_banhang_thoitrang?useUnicode=true&characterEncoding=UTF-8";
     private static final String USER = "root";
-    private static final String PASSWORD = "23082004";
+    private static final String PASSWORD = "504901";
 
     public static Connection getConnection() throws SQLException {
         try {
